1fbd0e0f1f73fada7815bf3d3e62aa39aadd2438
Dmitriy <fr80@list.ru>
2019-03-04 20:39:55 +0300

    feat: add function to calculate brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let obj = {};
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        obj[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    let resultArr = [];
+    let strToArr = str.split("");
+    for (let i = 0; i < strToArr.length; i++) {
+        if (resultArr.length === 0) {
+            resultArr.push(strToArr[i]);
+            continue;
+        }
+        if (strToArr[i] === obj[resultArr[resultArr.length - 1]]) {
+            resultArr.pop();
+        } else {
+            resultArr.push(strToArr[i]);
+        }
+    }
+    return resultArr.length === 0;
+};
